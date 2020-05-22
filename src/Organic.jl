7c49caaf598d01b170bfa37ff52a0bceba2d4f67
Tim Whitaker <camp4climber@gmail.com>
2020-05-22 15:33:50 -0600

    Parse two meta keywords

--- src/Organic.jl
+++ src/Organic.jl
@@ -3,7 +3,12 @@ module Organic
 export parse
 
 function parse(file::AbstractString)
-    meta = Dict("test" => 1)
+    f = read(file, String)
+
+    title = match(r"#+TITLE: (*.)", f)
+    description = match(r"#+DESCRIPTION: (*.)", f)
+
+    meta = Dict("title" => title, "description" => description)
     content = run(`pandoc $(file) -f org -t html`)
 
     return meta, content
