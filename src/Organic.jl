d8680a408133eab6faa060620df765ed41a6ac05
Tim Whitaker <camp4climber@gmail.com>
2020-05-22 16:06:25 -0600

    Fix regex

--- src/Organic.jl
+++ src/Organic.jl
@@ -5,11 +5,11 @@ export parse
 function parse(file::AbstractString)
     f = read(file, String)
 
-    title = match(r"#+TITLE: (*.)", f)
-    description = match(r"#+DESCRIPTION: (*.)", f)
+    title = match(r"#\+TITLE: (.*)", f).captures[1]
+    description = match(r"#\+DESCRIPTION: (.*)", f).captures[1]
 
     meta = Dict("title" => title, "description" => description)
-    content = run(`pandoc $(file) -f org -t html`)
+    content = read(`pandoc $(file) -f org -t html`, String)
     return meta, content
 end
 
