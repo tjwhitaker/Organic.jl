f8fdcc26efdae3fdf0e1cb405745b6bae67db971
Tim Whitaker <camp4climber@gmail.com>
2020-05-22 15:21:00 -0600

    Add simple pandoc function

--- src/Organic.jl
+++ src/Organic.jl
@@ -1,3 +1,10 @@
 module Organic
 
+export parse
+
+function parse(file::AbstractString)
+    meta = Dict("test" => 1)
+    content = run(`pandoc $(file) -f org -t html`)
+
+    return meta, content
 end
